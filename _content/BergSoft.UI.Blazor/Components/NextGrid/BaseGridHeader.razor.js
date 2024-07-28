69b82717d40bedc203916040234a5808a0f05c98
Bojan Nikolic <boki@bergsoft.net>
2024-07-28 23:05:43 +0200

    NextGrid changes

--- _content/BergSoft.UI.Blazor/Components/NextGrid/BaseGridHeader.razor.js
+++ _content/BergSoft.UI.Blazor/Components/NextGrid/BaseGridHeader.razor.js
@@ -1,6 +1,8 @@
 export function setupGridHeader(header, dotNetHelper) {
-    header.addEventListener('contextmenu', function handler(event) {
-        event.preventDefault();
-        dotNetHelper.invokeMethodAsync('OnRightClick', event.clientX, event.clientY);
-    });
+    if (header !== null) {
+        header.addEventListener('contextmenu', function handler(event) {
+            event.preventDefault();
+            dotNetHelper.invokeMethodAsync('OnRightClick', event.clientX, event.clientY);
+        });
+    }
 }
