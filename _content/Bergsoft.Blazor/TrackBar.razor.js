bbf47b7550245d2aa7e1fa68c78d35eeea458be7
Bojan Nikolic <boki@bergsoft.net>
2025-05-25 22:36:12 +0200

    New update

--- _content/Bergsoft.Blazor/TrackBar.razor.js
+++ _content/Bergsoft.Blazor/TrackBar.razor.js
@@ -1,9 +1,38 @@
 export function connect(dotnet, knob, currentPosition, min, max) {
     let isDragging = false;
 
+    let slide = knob.parentElement;
+    let progress = knob.previousElementSibling;
+    let boundingRect = slide.getBoundingClientRect();
+
     function calcKnobLeft(position) {
         return ((position - min) / (max - min)) * 100;
     }
+
+    function startDragging(e) {
+        isDragging = true;
+        knob.setPointerCapture(e.pointerId);
+    }
+
+    function stopDragging(e) {
+        isDragging = false;
+        knob.setPointerCapture(e.pointerId);
+    }
+
+    slide.addEventListener("pointerup", e => stopDragging(e));
+
+    slide.addEventListener("pointerdown", (e) => {
+        const relativeX = e.clientX - boundingRect.left;
+        const clampedX = Math.max(0, Math.min(relativeX, boundingRect.width));
+        const position = Math.round(clampedX / (boundingRect.width / (max - min)));
+
+        if (position >= min && position <= max) {
+            updateKnobAndProgress(position);
+            dotnet.invokeMethodAsync("PositionChangedAsync", position);
+        }
+
+        startDragging(e);
+    });
 
     knob.addEventListener("pointermove", (e) => {
         if (isDragging) {
@@ -24,10 +53,7 @@ export function connect(dotnet, knob, currentPosition, min, max) {
         e.currentTarget.setPointerCapture(e.pointerId);
     });
 
-    knob.addEventListener("pointerup", (e) => {
-        isDragging = false;
-        e.currentTarget.releasePointerCapture(e.pointerId);
-    });
+    knob.addEventListener("pointerup", e => stopDragging(e));
 
     function updateKnobAndProgress(value) {
         knob.style.left = `${calcKnobLeft(value)}%`;
@@ -35,9 +61,4 @@ export function connect(dotnet, knob, currentPosition, min, max) {
             progress.style.width = `${calcKnobLeft(value)}%`;
         }
     }
-
-    let slide = knob.parentElement;
-    let progress = knob.previousElementSibling;
-    let boundingRect = slide.getBoundingClientRect();
-//    updateKnobAndProgress(currentPosition);
 }
