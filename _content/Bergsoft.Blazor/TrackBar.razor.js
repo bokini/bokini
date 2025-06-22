8fad1ad88badec78c4ac9d4d7b105d702e1c571d
Bojan Nikolic <boki@bergsoft.net>
2025-06-22 18:47:10 +0200

    New update

--- _content/Bergsoft.Blazor/TrackBar.razor.js
+++ _content/Bergsoft.Blazor/TrackBar.razor.js
@@ -1,13 +1,11 @@
-export function connect(dotnet, knob, currentPosition, min, max) {
+export function connect(dotnet, knob, hasProgress, progressRef, min, max) {
     let isDragging = false;
 
-    let slide = knob.parentElement;
-    let progress = knob.previousElementSibling;
-    let boundingRect = slide.getBoundingClientRect();
+    let trackBar = knob.parentElement;
+    const bounds = trackBar.getBoundingClientRect();
+    const knobBounds = knob.getBoundingClientRect();
 
-    function calcKnobLeft(position) {
-        return ((position - min) / (max - min)) * 100;
-    }
+    const calcKnobLeft = position => ((position - min) / (max - min)) * 100;
 
     function startDragging(e) {
         isDragging = true;
@@ -19,12 +17,12 @@ export function connect(dotnet, knob, currentPosition, min, max) {
         knob.setPointerCapture(e.pointerId);
     }
 
-    slide.addEventListener("pointerup", e => stopDragging(e));
+    trackBar.addEventListener("pointerup", e => stopDragging(e));
 
-    slide.addEventListener("pointerdown", (e) => {
-        const relativeX = e.clientX - boundingRect.left;
-        const clampedX = Math.max(0, Math.min(relativeX, boundingRect.width));
-        const position = Math.round(clampedX / (boundingRect.width / (max - min)));
+    trackBar.addEventListener("pointerdown", (e) => {
+        const relativeX = e.clientX - bounds.left;
+        const clampedX = Math.max(0, Math.min(relativeX, bounds.width));
+        const position = Math.round(clampedX / (bounds.width / (max - min)));
 
         if (position >= min && position <= max) {
             updateKnobAndProgress(position);
@@ -36,18 +34,18 @@ export function connect(dotnet, knob, currentPosition, min, max) {
 
     knob.addEventListener("pointermove", (e) => {
         if (isDragging) {
-            const relativeX = e.clientX - boundingRect.left;
-            const clampedX = Math.max(0, Math.min(relativeX, boundingRect.width));
-            const position = Math.round(clampedX / (boundingRect.width / (max - min)));
+            const relativeX = e.clientX - bounds.left;
+            const clampedX = Math.max(0, Math.min(relativeX, bounds.width));
+            const position = Math.round(clampedX / (bounds.width / (max - min)));
                 
             if (position >= min && position <= max) {
-                updateKnobAndProgress(position);
+                //updateKnobAndProgress(position);
                 dotnet.invokeMethodAsync("PositionChangedAsync", position);
             }
         }
     });
 
-    knob.addEventListener("pointerdown", (e) => {
+    knob.addEventListener("pointerdown", e => {
         isDragging = true;
         e.currentTarget.setPointerCapture(e.pointerId);
     });
@@ -55,9 +53,9 @@ export function connect(dotnet, knob, currentPosition, min, max) {
     knob.addEventListener("pointerup", e => stopDragging(e));
 
     function updateKnobAndProgress(value) {
-        knob.style.left = `${calcKnobLeft(value)}%`;
-        if (progress) {
-            progress.style.width = `${calcKnobLeft(value)}%`;
+        knob.style.left = `calc(${calcKnobLeft(value)}% - ${knobBounds.height / 2}px)`;
+        if (hasProgress) {
+            progressRef.style.width = `${calcKnobLeft(value)}%`;
         }
     }
 }
