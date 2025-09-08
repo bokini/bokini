4b393d70ceb7c6db8ad5e183b591dfdf7de799f9
Bojan Nikolic <boki@bergsoft.net>
2025-09-08 23:24:14 +0200

    jhgjhg

--- _content/Bergsoft.Blazor/js/reordering.js
+++ _content/Bergsoft.Blazor/js/reordering.js
@@ -3,13 +3,14 @@ function setReordering(listbox, dotNet) {
     const style = getComputedStyle(listbox);
     let item = null;
     let dragIndex = null;
+    let targetIndex = null;
     listbox.addEventListener("pointerdown", (event) => {
         if (event.target instanceof HTMLElement) {
             if (event.target.classList.contains("drag-handle")) {
                 item = event.target.parentNode;
                 dragIndex = parseInt(item.dataset.index);
                 let draggedRect = item.getBoundingClientRect();
-                deltaY = event.pageY - draggedRect.top;
+                deltaY = event.pageY - draggedRect.top - listbox.scrollTop;
                 dotNet.invokeMethodAsync("HandleDragStart", dragIndex, calculateTop(event));
                 item.setPointerCapture(event.pointerId);
                 listbox.addEventListener("pointermove", onPointerMove);
@@ -23,7 +24,20 @@ function setReordering(listbox, dotNet) {
         return `${localY - deltaY}px`;
     }
     function onPointerMove(event) {
-        dotNet.invokeMethodAsync('OnDragStart', event.clientX, event.clientY);
+        event.preventDefault();
+        event.stopPropagation();
+        const items = [...listbox.querySelectorAll(".item:not(.dragging)")];
+        for (let item of items) {
+            const itemRect = item.getBoundingClientRect();
+            if (event.clientY < itemRect.top + itemRect.height / 2) {
+                if (item instanceof HTMLElement) {
+                    targetIndex = parseInt(item.dataset.index);
+                    dotNet.invokeMethodAsync("HandleDragOver", targetIndex, calculateTop(event));
+                    return;
+                }
+            }
+        }
+        dotNet.invokeMethodAsync("HandleDragOver", targetIndex, calculateTop(event));
     }
     function onPointerUp(event) {
         listbox.removeEventListener("pointermove", onPointerMove);
