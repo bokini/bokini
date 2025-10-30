a8654ced65fcc2c076455486a100cd79e15ef1e8
Bojan Nikolic <boki@bergsoft.net>
2025-10-30 18:44:28 +0100

    Fresh changes

--- _content/Bergsoft.Blazor.Grids/GridHeader.razor.js
+++
@@ -1,8 +0,0 @@
-export function setupGridHeader(header, dotNetHelper) {
-    if (header !== null) {
-        header.addEventListener('contextmenu', function handler(event) {
-            event.preventDefault();
-            dotNetHelper.invokeMethodAsync('OnRightClick', event.clientX, event.clientY);
-        });
-    }
-}
--- _content/Bergsoft.Blazor/js/controls.js
+++
@@ -1,7 +0,0 @@
-function setReordering(listbox, dotNet) {
-    let dragged = null;
-    let placeholder = null;
-    let deltaY = 0;
-    let start, destination = null;
-}
-//# sourceMappingURL=controls.js.map
