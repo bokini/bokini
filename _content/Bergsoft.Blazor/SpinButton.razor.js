48b7d8c175f31b50bde734e2c082c78a4815062a
Bojan Nikolic <boki@bergsoft.net>
2025-12-10 04:54:45 +0100

    trrt

--- _content/Bergsoft.Blazor/SpinButton.razor.js
+++
@@ -1,6 +0,0 @@
-export function addEvents(button) {
-    if (button) {
-        button.addEventListener("pointerdown", e => button.setPointerCapture(e.pointerId));
-        button.addEventListener("pointerup", e => button.releasePointerCapture(e.pointerId));
-    }
-}
